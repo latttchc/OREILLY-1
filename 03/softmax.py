d72447ca5cc1672cb3b1bc2410ac8433fa21b392
latttchc <iam.yuk2882@gmail.com>
2025-01-10 13:29:00 +0900

    text更新

--- 03/softmax.py
+++ 03/softmax.py
@@ -2,11 +2,9 @@ import numpy as np
 
 def softmax(a):
     c = np.max(a)
-    exp_a = np.exp(a - c) #指数関数
+    exp_a = np.exp(a - c) #指数関数の中では値の最大値を引く(overflow対策)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
 
     return y
-
-
 
